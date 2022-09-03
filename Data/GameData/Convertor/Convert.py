dc74ef037dd4d1770c7f4ad89180495e5e86b642
HSGameDevelop <105215347+HSGameDevelop@users.noreply.github.com>
2022-09-04 03:53:43 +0900

    ファイルコンバート用のUI回りをひとまずコミット

---
+++ Data/GameData/Convertor/Convert.py
@@ -0,0 +1,63 @@
+import os,sys
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog
+
+# ファイル指定の関数
+def filedialog_clicked():
+    fTyp = [("Excelファイル", "*.xlsx")]
+    iFile = os.path.abspath(os.path.dirname(__file__))
+    iFilePath = filedialog.askopenfilename(filetype = fTyp, initialdir = iFile)
+    entry2.set(iFilePath)
+
+# 実行ボタン押下時の実行関数
+def conductMain():
+    text = ""
+
+    filePath = entry2.get()
+
+    if filePath:
+        text += "ファイルパス：" + filePath
+
+    if text:
+        messagebox.showinfo("info", text)
+    else:
+        messagebox.showerror("error", "パスの指定がありません。")
+
+def CreateSelectFileMenu(tkInstance):
+    # ファイル選択Frameの作成
+    frameSelectFile = ttk.Frame(tkInstance, padding=10)
+    frameSelectFile.grid(row=2, column=1, sticky=E)
+
+    # 「ファイル選択」ラベルの作成
+    IFileLabel = ttk.Label(frameSelectFile, text="ファイル選択＞＞", padding=(5, 2))
+    IFileLabel.pack(side=LEFT)
+
+    # 「ファイル選択」エントリーの作成
+    entry2 = StringVar()
+    IFileEntry = ttk.Entry(frameSelectFile, textvariable=entry2, width=30)
+    IFileEntry.pack(side=LEFT)
+
+    # 「ファイル選択」ボタンの作成
+    IFileButton = ttk.Button(frameSelectFile, text="参照", command=filedialog_clicked)
+    IFileButton.pack(side=LEFT)
+
+if __name__ == "__main__":
+
+    # rootの作成
+    root = Tk()
+    # 画面タイトル
+    root.title('ExcelConvertor')
+
+    CreateSelectFileMenu(root)
+
+    # Frameの作成
+    frame3 = ttk.Frame(root, padding=10)
+    frame3.grid(row=5,column=1,sticky=W)
+
+    # 実行ボタンの設置
+    button1 = ttk.Button(frame3, text="実行", command=conductMain)
+    button1.pack(fill = "x", padx=30, side = "left")
+
+    root.mainloop()
