53ec48d5ffa87c723637ba5a7b6d8852f99e2edc
kokorochan0617 <kimushin0617@gmail.com>
2022-09-05 11:51:50 +0900

    扇形の描画修正とＵＲＬを追加

--- Display/plactice.py
+++ Display/plactice.py
@@ -8,7 +8,31 @@ root.title('windowPlactice')
 #これで描画できる
 canvas = tk.Canvas(root)
 canvas.pack()
-canvas.create_arc(100, 50, 300, 250, width=1, fill="#00ff00")
+
+# 扇形の座標
+# 左上(x1, y1)
+x1 = 10
+y1 = 10
+# 右下(x2, y2)
+x2 = 200
+y2 = 200
+# 作成の方法が四角形の左上と右下の座標から生成する。
+
+extent_type = 100
+
+#  扇形の作成
+canvas.create_arc(x1, y1, x2, y2, width=1, extent=extent_type, start=10)
+
+# 基準の点からのずれの分を戻すために初期座標のx1,y1をx1_1,x2_1・y1_1,y2_1にそれぞれ加算する
+# 左上(x1_1, y1_1)
+x1_1 = (x2 - x1) / 2 - 5 + x1
+y1_1 = (y2 - y1) / 2 - 5 + y1
+# 右下(x2_1, y2_1)
+x2_1 = (x2 - x1) / 2 + 5 + x1
+y2_1 = (y2 - y1) / 2 + 5 + y1
+
+#  円の作成
+canvas.create_oval(x1_1, y1_1, x2_1, y2_1, width=5, fill="#ff0000")
 
 
 # Canvasの作成
