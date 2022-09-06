5400deefa5dffde131392157a6bee71325e94c9f
HSGameDevelop <105215347+HSGameDevelop@users.noreply.github.com>
2022-09-06 22:46:21 +0900

    Excelコンバートツールで一部機能の仮実装

--- Data/GameData/Converter/Convert.py
+++ Data/GameData/Converter/Convert.py
@@ -5,11 +5,17 @@ from tkinter import messagebox
 from tkinter import filedialog
 import pathlib
 import shutil
+import tkinter
+from turtle import left
 from ExcelImporter import excelFileManager
 from binaryFileManager import binaryFileManager
 
 entrySelectFile = 0 # ファイル選択のエントリー
 isCanConver = False # コンバートできるかどうかのフラグ
+buttonConvert = None # コンバートボタン
+
+# 各種フラグ群
+sheetBln = {} # シート一覧チェックボックスのフラグ群
 
 # ファイル指定の関数
 def filedialog_clicked():
@@ -25,7 +31,6 @@ def ConverBinaryDataFromExcel(filePath):
     excelManager = excelFileManager()
     readData = excelManager.ReadExcelFile(filePath)
     
-    # バイナリファイルへの書き込み
     binaryManager = binaryFileManager()
     dir = os.path.dirname(filePath)
     path = pathlib.Path(filePath)
@@ -33,10 +38,6 @@ def ConverBinaryDataFromExcel(filePath):
     afterFileName = beforeFileName + ".bin"
     convertFilePath = dir + "/" + afterFileName
     binaryManager.WriteFileFromString(convertFilePath, readData.to_string())
-
-    # 確認用
-    readData = binaryManager.ReadFileToString(convertFilePath)
-    print(readData)
 
 # 実行ボタン押下時の実行関数
 def conductMain():
@@ -53,7 +54,31 @@ def conductMain():
         messagebox.showerror("error", "パスの指定がありません。")
 
 def CheckSelectFile():
-    return None
+    excelManager = excelFileManager()
+    sheetList = excelManager.ReadExcelSheetNames(entrySelectFile.get())
+
+    frameSheetLabel = ttk.Frame(window, padding=5)
+    frameSheetLabel.grid(row=2,column=0,sticky=NW)
+    labelSheet = ttk.Label(frameSheetLabel, text="シート一覧：", padding=(5, 2))
+    labelSheet.pack(side=LEFT)
+
+    frameSheet = ttk.Frame(window, padding=5)
+    frameSheet.grid(row=2,column=1,sticky=NW)
+    global sheetBln
+    for i in range(len(sheetList)):
+        sheetBln[i] = tkinter.BooleanVar()
+        chk = tkinter.Checkbutton(frameSheet, variable=sheetBln[i], text=sheetList[i], command=SelectSheeteNamesCheckBox) 
+        chk.grid(row=0,column=i)
+        
+
+def SelectSheeteNamesCheckBox():
+    global buttonConvert
+    buttonConvert['state'] = tkinter.DISABLED
+
+    for i in range(len(sheetBln)):
+        if sheetBln[i].get():
+            buttonConvert['state'] = tkinter.NORMAL 
+            break
     
 # ウィンドウを画面中央に作成
 def CreateCenterWindow(title, width, height):
@@ -75,19 +100,22 @@ def CreateCenterWindow(title, width, height):
 def CreateSelectFile(window):
     # ファイル選択Frameの作成
     frameSelectFile = ttk.Frame(window, padding=5)
-    frameSelectFile.grid(row=0, column=0,sticky=W+E)
+    frameSelectFile.grid(row=0, column=0,sticky=NW)
 
     # 「ファイル選択」ラベルの作成
-    IFileLabel = ttk.Label(frameSelectFile, text="ファイル選択：", padding=(5, 2))
+    IFileLabel = ttk.Label(frameSelectFile, text="ファイル選択：")
     IFileLabel.pack(side=LEFT)
+
+    frameSelectFile2 = ttk.Frame(window, padding=5)
+    frameSelectFile2.grid(row=0, column=1,sticky=NW)
 
     # 「ファイル選択」エントリーの作成
     entry = StringVar()
-    IFileEntry = ttk.Entry(frameSelectFile, state="readonly", textvariable=entry, width=60)
+    IFileEntry = ttk.Entry(frameSelectFile2, state="readonly", textvariable=entry, width=60)
     IFileEntry.pack(side=LEFT)
 
     # 「ファイル選択」ボタンの作成
-    IFileButton = ttk.Button(frameSelectFile, text="参照", command=filedialog_clicked)
+    IFileButton = ttk.Button(frameSelectFile2, text="参照", command=filedialog_clicked)
     IFileButton.pack(side=LEFT)
 
     return entry
@@ -95,15 +123,20 @@ def CreateSelectFile(window):
 # 各ボタンの作成
 def CreateButtons(window):
     # Frameの作成
-    frame3 = ttk.Frame(window, padding=5)
-    frame3.grid(row=1,column=0,sticky=W+E)
+    frameButtonCheck = ttk.Frame(window, padding=5)
+    frameButtonCheck.grid(row=1,column=0,sticky=NW)
 
     # 確認ボタンの設置
-    buttonCheck = ttk.Button(frame3, text="読み込み", command=conductMain)
+    buttonCheck = ttk.Button(frameButtonCheck, text="読み込み", command=CheckSelectFile)
     buttonCheck.pack(fill = "x", side = "left")
     
+    # Frameの作成
+    frameButtonConvert = ttk.Frame(window, padding=5)
+    frameButtonConvert.grid(row=1,column=1,sticky=NW)
+
     # 実行ボタンの設置
-    buttonConvert = ttk.Button(frame3, text="コンバート", command=conductMain)
+    global buttonConvert
+    buttonConvert = ttk.Button(frameButtonConvert, state="disable", text="コンバート", command=conductMain)
     buttonConvert.pack(fill = "x", side = "left")
 
 if __name__ == "__main__":
