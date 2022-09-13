fd7a0eed26591853a70eceebd6b0b0d3d1562f42
HSGameDevelop <105215347+HSGameDevelop@users.noreply.github.com>
2022-09-14 00:25:51 +0900

    一部コメントの追加

--- Script/Data/GameData.py
+++ Script/Data/GameData.py
@@ -4,9 +4,9 @@ sys.path.append('../../Script/Data/')
 from Script.Data.CharacterData import CharacterDataLoader
 
 class GameData:
+    characterData = []  # キャラクターデータ
+
     def LoadData(self):
+        # キャラクターデータの読み込み
         cdl = CharacterDataLoader()
         characterData = cdl.LoadData()
-        print (characterData)
-
-    characterData = []
