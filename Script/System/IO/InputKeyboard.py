0e499daa77781b96c1cec2992356040fa458df98
HSGameDevelop <105215347+HSGameDevelop@users.noreply.github.com>
2022-09-16 23:31:26 +0900

    キー入力：コメントアウトの追加

--- Script/System/IO/InputKeyboard.py
+++ Script/System/IO/InputKeyboard.py
@@ -14,14 +14,18 @@ class InputKeyboard:
         tkInstance.bind('<KeyPress>', self.KeyPush)
         tkInstance.bind('<KeyRelease>', self.KeyPop)
 
+    # キーを押下時の処理
     def KeyPush(self, e):
         self.key = e.keysym
     
+    # キーを離した時の処理
     def KeyPop(self, e):
         self.key = None
 
+    # 現在押しているキーを取得する
     def GetPushKey(self):
         return self.key
 
-    def CheckPushKey(self, key):
+    # 現在押しているキーが指定したキーかどうかを返す(true:押している/false:押していない)
+    def CheckPushKey(self, key : InputKeyList):
         return self.GetPushKey() == key
