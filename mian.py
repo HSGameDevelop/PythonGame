b6bde0d79c137561d75a5752ef9ead1316149781
HSGameDevelop <105215347+HSGameDevelop@users.noreply.github.com>
2022-09-17 00:26:53 +0900

    ゲームループの作成など

--- mian.py
+++ mian.py
@@ -9,22 +9,27 @@ from Script.Display.Game import Game
 gd = GameData()
 gd.LoadData()
 
+game = Game()
+
 print(gd.GetCharacterDataFromId(1).characterName)
 print(gd.GetWeaponDataFromId(1).weaponName)
 
 
 from pygame.locals import *
-# ゲーム画面を初期化 --- (*1)
+# ゲーム画面を初期化
 pygame.init()
 screen = pygame.display.set_mode((600, 400))
 white = (255,255,255)
 black = (0,0,0)
-# 繰り返し画面を描画 --- (*2)
+
+# ゲームループ
 while True:
-    # 背景と円を描画 --- (*3)
     screen.fill(black) # 背景を黒で塗りつぶす
-    pygame.draw.circle(screen, white, (300,200), 150) # 円を描画
-    # 画面を更新 --- (*4)
+
+    game.Update()
+    game.Draw()
+    
+    # 画面を更新
     pygame.display.update()
     # 終了イベントを確認 --- (*5)
     for event in pygame.event.get():
