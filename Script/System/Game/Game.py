d52fa7a5b58fb63177bf4dac3c31c23a62b99d4a
HSGameDevelop <105215347+HSGameDevelop@users.noreply.github.com>
2022-09-18 01:29:19 +0900

    ゲームの各シーンのベースとなるクラスを用意

--- Script/System/Game/Game.py
+++ Script/System/Game/Game.py
@@ -4,6 +4,7 @@ from Script.Display.GameDraw import GameDraw
 
 sys.path.append('../../System/')
 from Script.System.Game.PgLib import PgLib
+from Script.System.Game.GameSequenceBase import GameSequenceBase
 
 sys.path.append('../../System/IO/')
 from Script.System.IO.InputManager import InputManager
@@ -15,6 +16,7 @@ HEIGHT = 480
 class Game:    
     def __init__(self):
         self.pgLib = PgLib(WIDTH, HEIGHT)
+        self.gameSequence = GameSequenceBase()
         #self.tkIntance = tkIntance
         #self.gd = GameDraw(self.tkIntance)
         #self.inputManager = InputManager(tkIntance)
@@ -22,7 +24,7 @@ class Game:
 
     # ゲーム全般の更新処理
     def Update(self):
-        #gameUpdate.Update()
+        self.gameSequence.Update()
         return
 
     # ゲーム全般の描画処理
@@ -30,7 +32,7 @@ class Game:
         # 描画開始
         self.pgLib.DrawStart()
 
-        #gameDraw.Update()
+        self.gameSequence.Update()
 
         # 描画終了
         self.pgLib.DrawEnd()
